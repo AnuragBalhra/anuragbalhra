b2256ee834e0e60f91636edf1a670dfc15fbb38e
AnuragBalhra <anurag.1@iitj.ac.in>
2024-11-07 11:44:28 +0530

    Update hero.jsx

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -57,7 +57,7 @@ export default function HeroHome() {
                             <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={avatar_image} alt="Bonnie image"/>
                             <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Anurag Balhra</h5>
                             <span class="text-sm text-gray-500 dark:text-gray-400">Software Engineer</span>
-                            <div class="flex mt-4 space-x-3 md:mt-6">
+                            <div class="flex flex-col md:flex-row mt-4 space-x-3 md:mt-6">
                                 <a
                                     href={resume_pdf}
                                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
