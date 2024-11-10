89f9ad6ff87b1c3c6eb027429358458c878025c7
AnuragBalhra <anurag.1@iitj.ac.in>
2024-11-10 23:51:54 +0530

    Update hero.jsx

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -51,13 +51,13 @@ export default function HeroHome() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-1 justify-center">
+                <div className="flex flex-1 justify-center w-full">
                     <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 py-10">
                         <div class="flex flex-col items-center pb-10">
                             <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src={avatar_image} alt="Bonnie image"/>
                             <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">Anurag Balhra</h5>
                             <span class="text-sm text-gray-500 dark:text-gray-400">Software Engineer</span>
-                            <div class="flex flex-col md:flex-row mt-4 space-x-3 md:mt-6">
+                            <div class="flex flex-col md:flex-row mt-4 md:mt-6 gap-3 md:gap-1">
                                 <a
                                     href={resume_pdf}
                                     class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
